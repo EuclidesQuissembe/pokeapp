d7c8a0054881f59d77e2579c5f3be86e1dcee55a
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 20:17:23 +0100

    :zap: Passing pagination parameter

--- src/store/ducks/pokemons/actions.ts
+++ src/store/ducks/pokemons/actions.ts
@@ -1,9 +1,10 @@
 import { action } from "typesafe-actions";
-import { PokemonsTypes, PokemonsState } from "./types";
+import { PokemonsTypes, PokemonsState, Pagination } from "./types";
 
-export const loadRequest = () => action(PokemonsTypes.LOAD_REQUEST);
+export const loadRequest = (pagination: Pagination) =>
+  action(PokemonsTypes.LOAD_REQUEST, pagination);
 
-export const loadSuccess = (response: PokemonsState) =>
+export const loadSuccess = (response: PokemonsState | undefined) =>
   action(PokemonsTypes.LAOD_SUCCESS, response);
 
 export const loadFailure = () => action(PokemonsTypes.LOAD_FAILURE);
