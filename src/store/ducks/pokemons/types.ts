450a3fa6faf13aed38616f57769750cb5a2c160e
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 11:26:36 +0100

    :sparkles: Pokemons types

---
+++ src/store/ducks/pokemons/types.ts
@@ -0,0 +1,33 @@
+export enum PokemonsTypes {
+  LOAD_REQUEST = "@pokemons/LOAD_REQUEST",
+  LAOD_SUCCESS = "@pokemons/LOAD_SUCCESS",
+  LOAD_FAILURE = "@pokemons/LOAD_FAILURE",
+}
+
+export interface Pokemon {
+  id: number;
+  url: string;
+  name: string;
+  image: string;
+  artwork: string;
+  dreamworld: string;
+}
+
+export interface PokemonsResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  message: string;
+  status: boolean;
+  results: Pokemon[];
+}
+
+export interface PokemonsData {
+  pokemons: PokemonsResponse;
+}
+
+export interface PokemonsState {
+  readonly data: PokemonsData;
+  readonly loading: boolean;
+  readonly error: boolean;
+}
