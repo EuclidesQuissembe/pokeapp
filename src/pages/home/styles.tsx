0d2010331489c354c8abbfdcab9214eabeb0f579
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 20:21:06 +0100

    :zap: Container and Button

--- src/pages/home/styles.tsx
+++ src/pages/home/styles.tsx
@@ -1,5 +1,10 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
 export const Row = styled.div`
   display: flex;
   flex-direction: row;
@@ -9,7 +14,18 @@ export const Row = styled.div`
 export const Col = styled.div`
   width: 100%;
   flex: 1;
-
   @media (max-width: 512px) {
+  }
+`;
+
+export const Button = styled.button`
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  background-color: blue;
+  color: white;
+
+  &:hover {
+    cursor: pointer;
   }
 `;
