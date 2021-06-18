ce6da73136977ba07017e0e233ae6322ea7398b0
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 20:18:30 +0100

    :zap: Remove static data`

--- src/store/ducks/pokemons/index.ts
+++ src/store/ducks/pokemons/index.ts
@@ -8,30 +8,7 @@ const INITIAL_STATE: PokemonsState = {
       next: null,
       previous: null,
       message: "",
-      results: [
-        {
-          id: 1,
-          url: "https://pokeapi.co/api/v2/pokemon/1/",
-          name: "bulbasaur",
-          image:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png",
-          artwork:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-          dreamworld:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg",
-        },
-        {
-          id: 2,
-          url: "https://pokeapi.co/api/v2/pokemon/2/",
-          name: "ivysaur",
-          image:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png",
-          artwork:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/2.png",
-          dreamworld:
-            "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/2.svg",
-        },
-      ],
+      results: [],
       status: false,
     },
   },
