377eede19ecbc27c15f653eae0d958adc82d1b5f
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 20:18:52 +0100

    :sparkles: Redux saga implemented

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
-import { createStore, Store } from "redux";
+import { createStore, applyMiddleware, Store } from "redux";
+import createSagaMiddleware from "redux-saga";
 import { PokemonsState } from "./ducks/pokemons/types";
 
 import rootReducer from "./ducks/rootReducer";
+import rootSaga from "./ducks/rootSaga";
 
 export interface ApplicationState {
-  pokemons: PokemonsState;
+  data: PokemonsState;
 }
 
-const store: Store<ApplicationState> = createStore(rootReducer);
+const sagaMiddleware = createSagaMiddleware();
+
+const store: Store<ApplicationState> = createStore(
+  rootReducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
