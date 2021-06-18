1b0ee8e63f67fa5761fc1f1c0c3c9b6d8cd6134d
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 20:23:00 +0100

    :art: Container rstyle

---
+++ src/components/footer/styles.tsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: fixed;
+  width: 100%;
+  bottom: 30px;
+  display: flex;
+  justify-content: space-between;
+`;
