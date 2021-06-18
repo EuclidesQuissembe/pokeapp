774a4ccdad724cda5279ffbd7f5cfc75e4c345ae
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-18 20:17:00 +0100

    :sparkles: Axios implemented

---
+++ src/store/ducks/pokemons/sagas.ts
@@ -0,0 +1,41 @@
+import { call, put, takeLatest } from "redux-saga/effects";
+import api from "../../../services/api";
+
+import { PokemonsTypes } from "./types";
+import { loadSuccess, loadFailure } from "./actions";
+
+function* load({ payload }: any): any {
+  try {
+    const response = yield call(api.post, "/", {
+      query: `
+        query($limit: Int, $offset: Int) {
+          pokemons(limit: $limit, offset: $offset) {
+            count
+            next
+            previous
+            status
+            message
+            results {
+              id
+              url
+              name
+              image
+              artwork
+              dreamworld
+            }
+          }
+        }
+      `,
+      variables: {
+        limit: payload.limit,
+        offset: payload.offset,
+      },
+    });
+
+    yield put(loadSuccess(response.data));
+  } catch (err) {
+    yield put(loadFailure());
+  }
+}
+
+export default takeLatest(PokemonsTypes.LOAD_REQUEST, load);
