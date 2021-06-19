3aa4d3dcddd1a07773ab005213b3cf7d3cedba63
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-19 05:33:32 +0100

    :zap: Pokedexes route added

--- src/routes.tsx
+++ src/routes.tsx
@@ -5,11 +5,13 @@ import { Route, Routes } from "react-router-dom";
  * Pages
  */
 import Home from "./pages/home";
+import Pokedex from "./pages/pokedex";
 
 const RouteContainer: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/pokedexes" element={<Pokedex />} />
     </Routes>
   );
 };
