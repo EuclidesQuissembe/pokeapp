15f36bd53e5088db61832d811b472294ee55672f
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-19 05:27:58 +0100

    :hammer: Add pokedex reducer

--- src/store/ducks/rootSaga.ts
+++ src/store/ducks/rootSaga.ts
@@ -1,7 +1,8 @@
 import { all } from "redux-saga/effects";
 
 import pokemons from "./pokemons/sagas";
+import pokedexes from "./pokedex/sagas";
 
 export default function* rootSaga(): any {
-  return yield all([pokemons]);
+  return yield all([pokemons, pokedexes]);
 }
