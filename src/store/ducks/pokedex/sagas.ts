f9046babb270a5c5cfa3eb00a998b33d2752f4f1
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-19 05:26:42 +0100

    :sparkles: Pokedex saga

---
+++ src/store/ducks/pokedex/sagas.ts
@@ -0,0 +1,61 @@
+import { all, call, takeLatest, put } from "redux-saga/effects";
+import api from "../../../services/api";
+
+import { loadFailure, loadSuccess } from "./actions";
+
+import { Constants } from "../../../utils/constants";
+import { Pokedex } from "./types";
+import { PokedexTypes } from "../pokedex/types";
+import { addToCollectionSuccess } from "./actions";
+
+function* load() {
+  try {
+    const response = localStorage.getItem(Constants.POKEDEXES);
+
+    if (response) {
+      const pokedexes: Pokedex[] = JSON.parse(response);
+
+      yield put(loadSuccess(pokedexes));
+    }
+  } catch (err) {
+    yield put(loadFailure());
+  }
+}
+
+function* addToCollection({ payload }: any): any {
+  try {
+    const response = yield call(api.post, "/", {
+      query: `
+        query pokemon($name: String!) {
+          pokemon(name: $name) {
+            id
+            name
+            sprites {
+              front_default
+            }
+            moves {
+              move {
+                name
+              }
+            }
+            types {
+              type {
+                name
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        name: payload,
+      },
+    });
+
+    yield put(addToCollectionSuccess(response.data.data));
+  } catch (err) {}
+}
+
+export default all([
+  takeLatest(PokedexTypes.LOAD_REQUEST, load),
+  takeLatest(PokedexTypes.ADD_TO_COLLECTION, addToCollection),
+]);
