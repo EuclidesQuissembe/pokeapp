4d0aef4603781c8b6f7ec224be66c644bc8ff93c
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-19 05:28:17 +0100

    :hammer: Pokedex reducer added

--- src/store/ducks/rootReducer.ts
+++ src/store/ducks/rootReducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 
+import pokedex from "./pokedex";
 import pokemons from "./pokemons";
 
 export default combineReducers({
   data: pokemons,
+  pokedex,
 });
