c6c218f4c3fb2ca38884565c30f67b69bd22fcfe
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2021-06-19 05:34:05 +0100

    :zap: Pokemon detail page

---
+++ src/pages/detail/index.tsx
@@ -0,0 +1,10 @@
+import React, { useState, useEffect, useCallback } from "react";
+import { useParams } from "react-router-dom";
+
+import { Container } from "./styles";
+
+const Detail: React.FC = () => {
+  return <Container>Detalhe</Container>;
+};
+
+export default Detail;
